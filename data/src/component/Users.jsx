b38f454fd832c6de62720168f4e1f6dc35d34a4b
RakshanaR354 <144024500+RakshanaR354@users.noreply.github.com>
2023-12-30 13:19:44 +0530

    main

--- data/src/component/Users.jsx
+++ data/src/component/Users.jsx
@@ -1,31 +1,53 @@
 import React from "react";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 
 const Users = () =>{
-    const [users,setUsers] = useState([]);
+    const [items,setItems] = useState([])
+    const [column,setColumn] = useState([])
 
-const loadUsers = async() =>{
-    const response = await fetch("https://api.github.com/users");
-    const jsonResponse = await response.json();
-    setUsers(jsonResponse);
-}
+    
+          useEffect(()=>{
+           fetch("https://reqres.in/api/users?page=1")
+             .then(res=>res.json())
+             .then(data=>{
+                setColumn(Object.keys(data.data[0]))
+                setItems(data.data)
+             })
+        
+          },[])
 
-return(
-    <div>
-         <h1>Hello All</h1>
-         <button onClick={loadUsers}>Get Data</button>
-         <h2>Users:</h2>
-         <ul>
-            {users.map(({id,login,avatar_url})=>(
-                <li key={id}>
-                    Name:{login}
-                    Avator:{avatar_url}<br></br>
-                </li>
-            ))}
-         </ul>
-    </div>
-);
+          console.log(items)
+          
+     
+
+    return(
+        <div> 
+            <div>
+                {
+                    items.map(({id,avatar,first_name,last_name,email})=>{
+                        return(
+                        <div>
+                            <table className="tables">
+                                <tr key={id}>
+                                   <td>{id}</td>
+                                   <td>{email}</td>
+                                   <td>{first_name}</td>
+                                   <td>{last_name}</td>
+                                   <td><img src={avatar} alt={first_name}/></td>
+                                </tr>
+                            </table>
+                        </div>
+                            
+
+                        );
+                        
+                    })
+                }
+            </div>
+           
+        </div>
+    );
 }
 
 export default Users;
